390df2e451350aa7fef9cbbb9f04301f6c3e75dd
Star-Tube <50463083+Star-Tube@users.noreply.github.com>
2021-12-15 04:38:59 +1100

    Update _PnP.py

--- dist/src/Politics_and_Python/_PnP.py
+++ dist/src/Politics_and_Python/_PnP.py
@@ -5,7 +5,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
-# Checking if the push system is functioning again
+# Checking if the push system is functioning again again
 
 # Set Global Variables
 _Key = ""
